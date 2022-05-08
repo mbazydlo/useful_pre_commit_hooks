7718d1f6f744f0106dfdf3066d7e48a6f11614ba
Marcin Bazydło <marcin.p.bazydlo@gmail.com>
2022-05-08 20:59:09 +0200

    Added check_requirements_versions hook and deleted check_parameters_typing as will be different approach

---
+++ README.md
@@ -0,0 +1,5 @@
+Additional hooks for a great tool called pre-commit made by 
+Anthony Sottile - https://github.com/asottile
+### Developed hooks:
+check-requirements-versions: Checks does every of requirements in requirements*.txt
+file in a project have provided version (equal, greater than etc. )
---
+++ pre_commit_hooks/check_requirements_versions.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import argparse
+import re
+
+PATTERN = re.compile('(={2,3}|!=|~=|>=?|<=?).+')
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to check.')
+    args = parser.parse_args()
+
+    return_value = 0
+
+    for filename in args.filenames:
+        with open(filename, mode='r') as file:
+            for requirement in file.readlines():
+                if not PATTERN.search(requirement):
+                    print(f'Requirement library does not contain version info: {requirement}')
+                    return_value = 1
+    return return_value
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
